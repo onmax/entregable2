bfde61e75f52bac5fd17f00d3fdad3849d4711ef
Máximo J. García Martínez <maximo.garcia.martinez@alumnos.upm.es>
2017-05-11 12:46:07 +0200

    Finalizado

--- src/lotos/Historico.java
+++ src/lotos/Historico.java
@@ -2,61 +2,74 @@ package lotos;
 
 import fechas.*;
 import list.ArrayList;
+
 /**
  * Implementación de un Histórico de Sorteo(s)<br>
  * {@code Historico = Lista(Sorteo)<br>
+ * 
  * @since 17/02/2017
- * @version 1.1.1         22/03/2017  JGF
+ * @version 1.1.1 22/03/2017 JGF
  * @author JMB & JGF
  */
-public class Historico 
-{ 
-  private ArrayList<Sorteo> sorteos;
-  
-  public Historico ()
-  {
-    this.sorteos = new ArrayList<lotos.Sorteo>();  
-  }
-  public Sorteo get (int index) 
-  {
-    return sorteos.get(index);
-  }
-  public int size () 
-  {
-    return sorteos.size(); 
-  }
-  public void add (int index, Sorteo sorteo)
-  {
-    sorteos.add(index, sorteo);
-  }
-  /*
-   * POST: Visualizador del Historico.
-   */
-  public String toString ()
-  {
-    return sorteos.toString();
-  }
-  /**
-   * POST: Determina si fecha está en el intervalo 
-   *       [fecha1, fecha2].
-   */         
-  private static boolean estaEnIntervalo (Fecha fecha, 
-                                   Fecha fecha1, 
-                                   Fecha fecha2) { 
-    return fecha1.esMenorOIgual(fecha) && 
-           fecha.esMenorOIgual(fecha2);
-  }  
-  /**
-   * PRE: fecha1<=fecha2
-   * POST: Calcula las frecuencias de los reintegros en un intervalo
-   *       de fechas [fecha1, fecha2] en sorteos de la primitiva.
-   */
-  public int[] frecuenciasReintegro (Fecha fecha1, Fecha fecha2)
-  {
-    //TODO
-	
-	
-    return null;
-  }
+public class Historico {
+	private ArrayList<Sorteo> sorteos;
+
+	public Historico() {
+		this.sorteos = new ArrayList<lotos.Sorteo>();
+	}
+
+	public Sorteo get(int index) {
+		return sorteos.get(index);
+	}
+
+	public int size() {
+		return sorteos.size();
+	}
+
+	public void add(int index, Sorteo sorteo) {
+		sorteos.add(index, sorteo);
+	}
+
+	/*
+	 * POST: Visualizador del Historico.
+	 */
+	public String toString() {
+		return sorteos.toString();
+	}
+
+	/**
+	 * POST: Determina si fecha está en el intervalo [fecha1, fecha2].
+	 */
+	private static boolean estaEnIntervalo(Fecha fecha, Fecha fecha1, Fecha fecha2) {
+		return fecha1.esMenorOIgual(fecha) && fecha.esMenorOIgual(fecha2);
+	}
+
+	/**
+	 * PRE: fecha1<=fecha2 POST: Calcula las frecuencias de los reintegros en un
+	 * intervalo de fechas [fecha1, fecha2] en sorteos de la primitiva.
+	 */
+	public int[] frecuenciasReintegro(Fecha fecha1, Fecha fecha2) {
+		int dia1 = fecha1.getDia();
+		int mes1 = fecha1.getMes();
+		int anio1 = fecha1.getAnio();
+		int dia2 = fecha2.getDia();
+		int mes2 = fecha2.getMes();
+		int anio2 = fecha2.getAnio();
+
+		int[] frecuenciasReintegro = new int[10];
+
+		for (int i = 0; i < this.sorteos.size(); i++) {
+			if (this.sorteos.get(i).getLoto() instanceof Primitiva) {
+
+				Primitiva prim = (Primitiva) this.sorteos.get(i).getLoto();
+				Fecha fechaFinal = this.sorteos.get(i).getFecha();
+				if (estaEnIntervalo(fechaFinal, fecha1, fecha2)) {
+					frecuenciasReintegro[prim.getReintegro().getNumero()]++;
+				}
+
+			}
+		}
+		return frecuenciasReintegro;
+	}
 
 }
--- src/lotos/Loto.java
+++ src/lotos/Loto.java
@@ -4,182 +4,176 @@ import anotacion.Programacion2;
 import lotos.excepciones.BadSize;
 import lotos.excepciones.FueraDeRango;
 
-@Programacion2 (
-		nombreAutor1 = "Ra鷏",
-		apellidoAutor1 = "Carbajosa Gonz醠ez",
-		emailUPMAutor1 = "raul.cgonzalez@alumnos.upm.es",
-		nombreAutor2 = "M醲imo",
-		apellidoAutor2 = "Garc韆 Mart韓ez", 
-		emailUPMAutor2 = "maximo.garcia.martinez@alumnos.upm.es"
-		)
-
+@Programacion2(nombreAutor1 = "Ra鷏", apellidoAutor1 = "Carbajosa Gonz醠ez", emailUPMAutor1 = "raul.cgonzalez@alumnos.upm.es", nombreAutor2 = "M醲imo", apellidoAutor2 = "Garc韆 Mart韓ez", emailUPMAutor2 = "maximo.garcia.martinez@alumnos.upm.es")
 
 /**
  * Implementaci贸n de una Loto<br>
  * {@code Loto = (Entero[] numeros,
  *                Entero min, Entero max)}<br>
+ * 
  * @since 3/5/2016
  * @version 1.0
  * @author JMB & JGF
  */
-public class Loto
-{
-  private int[] numeros;
-  private int min, max;       // Rango de los numeros
-  /*
-   * PRE: longitud(numeros)=numerosSorteo /\ todosEnRango (numeros, min, max)   
-   * POST:  
-   *   @throws BadSize si longitud(numeros)!=numerosSorteo
-   *   @throws FueraDeRango si NO todosEnRango (numeros, min, max)
-   *   eoc --> Construye una Loto
-   */
-  public Loto (int numerosSorteo, int[] numeros, int min, int max)
-    throws BadSize, FueraDeRango
-  {
-    //TODO
-	 
-  }
-  /*
-   * POST: resultado = min<=n<=max
-   */
-  private static boolean enRango (int n, int min, int max)
-  { 
-    return min<=n && n<=max;
-  }
-  /*
-   * POST: Determina si todos los <numeros> estan en el
-   *       rango [min, max]
-   */
-  private static boolean todosEnRango (int[] numeros, int min, int max)
-  { 
-    //TODO
-	
-    return true;
-  }
-  /*
-   * POST: Manejador del constructor de Loto.
-   */
-  public static Loto consLoto (int numero, int[] numeros, int min, int max)
-  {
-    Loto loto = null;
-    try
-    {
-      loto = new Loto(numero, numeros, min, max);
-    }
-    catch (FueraDeRango e)
-    {
-    	System.err.println("Error al crear una instancia de Loto. "+ e.getMessage());
-    }
-    catch (BadSize e)
-    {
-    	System.err.println("Error al crear una instancia de Loto. "+ e.getMessage());
-    }
-    return loto;
-  }
-    /*
-   * POST: resultado es el limite inferior del rango de los numeros
-   */
-  public int getMin ()
-  {
-    return min;
-  }
-  /*
-   * POST: resultado es el limite superior del rango de los numeros
-   */
-  public int getMax ()
-  {
-    return max;
-  }
-  /*
-   * POST: Proporciona los numeros de Loto
-   */
-  public int[] getNumeros ()
-  {
-    return numeros;
-  }
-  /*
-   * PRE: pos IN [0,size()-1]
-   * POST: devuelve el elemento del objeto que esta en la 
-   *       posici贸n "pos".
-   */
-  public int get (int pos)
-  {
-    int resultado = (pos >=0 && pos < this.size())?
-    	 numeros[pos]: -1;
-    return resultado;
-  }
-  /*
-   * PRE: pos IN [0,size()-1]
-   * POST: modifica el elemento del objeto que esta en la posici贸n "pos".
-   */
-  public void set (int pos, int elem)
-  {
-    numeros[pos] = elem;
-  }
-  /*
-   * POST: Resultado es el tama帽o del objeto.
-   */
-  public int size()
-  {
-    return numeros.length;
-  }
+public class Loto {
+	private int[] numeros;
+	private int min, max; // Rango de los numeros
+	/*
+	 * PRE: longitud(numeros)=numerosSorteo /\ todosEnRango (numeros, min, max)
+	 * POST:
+	 * 
+	 * @throws BadSize si longitud(numeros)!=numerosSorteo
+	 * 
+	 * @throws FueraDeRango si NO todosEnRango (numeros, min, max) eoc -->
+	 * Construye una Loto
+	 */
 
-  /*
-   * PRE: ganadora.size() == this.size()
-   * POST: Decide si la Loto es igual a la "ganadora".
-   */
-  public boolean esGanadora (Loto ganadora)
-  {
-    return this.mismosNumeros(ganadora);
-  }
-  /*
-   * POST: Decide si "x" forma parte de la Loto
-   */
-  public boolean aparece (int buscado)
-  {
-    int i = 0;
-    boolean esta = false;
-    while ((i < numeros.length) && !esta)
-      if (buscado == numeros[i])
-      esta = true;
-    else
-      i = i + 1;
-    return esta;
-  }
-/**
- * POST: resultado es cierto si "this" es igual a "otra" (mismos
- *       elementos en las mismas posiciones) y es falso e.o.c.
- */
-  private boolean mismosNumeros (Loto otra)
-  {
-	 boolean iguales = this.size() == otra.size();
-	 for (int i=0;i<this.size() && iguales;i++){
-	    	iguales = this.numeros[i] == otra.numeros[i];
-	 }
-	 return iguales;
-  }
-   /*
-   * POST: Decide si "loto" es igual a esta Loto
-   */
-  public boolean esIgual (Loto loto)
-  {
-    return this.mismosNumeros(loto) &&
-           min==loto.getMin() &&
-           max==loto.getMax();
-  }
-  /*
-   * POST: resultado es la cadena de caracteres formada por los
-   *       atributos del objeto
-   */
-  public String toString ()
-  {
-    String resultado = "[";
-    for (int i = 0; i < numeros.length; i++)
-    {
-      resultado = resultado + numeros[i];
-      if (i < numeros.length-1)
-        resultado = resultado + ",";
-    }
-    return resultado + "]";
-  }
+	public Loto(int numerosSorteo, int[] numeros, int min, int max) throws BadSize, FueraDeRango {
+		// TODO
+		if (numeros.length != numerosSorteo) {
+			throw new BadSize("La longitud no es apropiada");
+
+		}
+
+		else if (!todosEnRango(this.numeros, this.min, this.max)) {
+			throw new FueraDeRango("El n鷐ero escapa el rango de Loto");
+		}
+
+		else {
+			this.numeros = new int[numerosSorteo];
+			this.min = min;
+			this.max = max;
+		}
+	}
+
+	/*
+	 * POST: resultado = min<=n<=max
+	 */
+	private static boolean enRango(int n, int min, int max) {
+		return min <= n && n <= max;
+	}
+
+	/*
+	 * POST: Determina si todos los <numeros> estan en el rango [min, max]
+	 */
+	private static boolean todosEnRango(int[] numeros, int min, int max) {
+		// TODO
+
+		return true;
+	}
+
+	/*
+	 * POST: Manejador del constructor de Loto.
+	 */
+	public static Loto consLoto(int numero, int[] numeros, int min, int max) {
+		Loto loto = null;
+		try {
+			loto = new Loto(numero, numeros, min, max);
+		} catch (FueraDeRango e) {
+			System.err.println("Error al crear una instancia de Loto. " + e.getMessage());
+		} catch (BadSize e) {
+			System.err.println("Error al crear una instancia de Loto. " + e.getMessage());
+		}
+		return loto;
+	}
+
+	/*
+	 * POST: resultado es el limite inferior del rango de los numeros
+	 */
+	public int getMin() {
+		return min;
+	}
+
+	/*
+	 * POST: resultado es el limite superior del rango de los numeros
+	 */
+	public int getMax() {
+		return max;
+	}
+
+	/*
+	 * POST: Proporciona los numeros de Loto
+	 */
+	public int[] getNumeros() {
+		return numeros;
+	}
+
+	/*
+	 * PRE: pos IN [0,size()-1] POST: devuelve el elemento del objeto que esta
+	 * en la posici贸n "pos".
+	 */
+	public int get(int pos) {
+		int resultado = (pos >= 0 && pos < this.size()) ? numeros[pos] : -1;
+		return resultado;
+	}
+
+	/*
+	 * PRE: pos IN [0,size()-1] POST: modifica el elemento del objeto que esta
+	 * en la posici贸n "pos".
+	 */
+	public void set(int pos, int elem) {
+		numeros[pos] = elem;
+	}
+
+	/*
+	 * POST: Resultado es el tama帽o del objeto.
+	 */
+	public int size() {
+		return numeros.length;
+	}
+
+	/*
+	 * PRE: ganadora.size() == this.size() POST: Decide si la Loto es igual a la
+	 * "ganadora".
+	 */
+	public boolean esGanadora(Loto ganadora) {
+		return this.mismosNumeros(ganadora);
+	}
+
+	/*
+	 * POST: Decide si "x" forma parte de la Loto
+	 */
+	public boolean aparece(int buscado) {
+		int i = 0;
+		boolean esta = false;
+		while ((i < numeros.length) && !esta)
+			if (buscado == numeros[i])
+				esta = true;
+			else
+				i = i + 1;
+		return esta;
+	}
+
+	/**
+	 * POST: resultado es cierto si "this" es igual a "otra" (mismos elementos
+	 * en las mismas posiciones) y es falso e.o.c.
+	 */
+	private boolean mismosNumeros(Loto otra) {
+		boolean iguales = this.size() == otra.size();
+		for (int i = 0; i < this.size() && iguales; i++) {
+			iguales = this.numeros[i] == otra.numeros[i];
+		}
+		return iguales;
+	}
+
+	/*
+	 * POST: Decide si "loto" es igual a esta Loto
+	 */
+	public boolean esIgual(Loto loto) {
+		return this.mismosNumeros(loto) && min == loto.getMin() && max == loto.getMax();
+	}
+
+	/*
+	 * POST: resultado es la cadena de caracteres formada por los atributos del
+	 * objeto
+	 */
+	public String toString() {
+		String resultado = "[";
+		for (int i = 0; i < numeros.length; i++) {
+			resultado = resultado + numeros[i];
+			if (i < numeros.length - 1)
+				resultado = resultado + ",";
+		}
+		return resultado + "]";
+	}
 }
